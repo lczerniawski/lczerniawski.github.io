747cab182e22478d7ce7f257c4f454dadebaa5eb
Lukasz Czerniawski <l.czerniawski@icloud.com>
2024-08-06 21:54:35 +0200

    Add dark theme to rest of the pages

--- 404.tsx
+++ 404.tsx
@@ -6,12 +6,12 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
       <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-orange-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-slate-100 sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600 dark:text-slate-400">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="/"
-              className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+              className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
             >
               Go back home
             </a>
--- about-me.tsx
+++ about-me.tsx
@@ -7,39 +7,39 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
     <section class="p-3 my-auto mx-auto max-w-3xl rounded-2xl sm:p-9 md:p-16 lg:mt-6 print:border-0 page print:max-w-letter print:max-h-letter print:mx-0 print:my-o xsm:p-8 print:bg-white md:max-w-letter md:h-letter lg:h-letter">
       <header class="inline-flex justify-between items-baseline mb-2 w-full align-top border-b-4 border-gray-300">
         <section class="block">
-          <h1 class="mb-0 text-5xl font-bold text-gray-900">Łukasz Czerniawski</h1>
-          <h2 class="m-0 ml-2 text-2xl font-semibold text-gray-900 leading-snugish">Junior Architect</h2>
-          <h3 class="m-0 mt-2 ml-2 pb-7 text-xl font-semibold text-gray-600 leading-snugish">Gdynia, Poland</h3>
+          <h1 class="mb-0 text-5xl font-bold text-gray-900 dark:text-slate-100">Łukasz Czerniawski</h1>
+          <h2 class="m-0 ml-2 text-2xl font-semibold text-gray-900 dark:text-slate-100 leading-snugish">Junior Architect</h2>
+          <h3 class="m-0 mt-2 ml-2 pb-7 text-xl font-semibold text-gray-600 dark:text-slate-400 leading-snugish">Gdynia, Poland</h3>
         </section>
       </header>
       <section class="col-gap-8 print:col-count-2 print:h-letter-col-full col-fill-balance md:col-count-2 md:h-letter-col-full">
         <section class="flex-col">
           <section class="pb-0 mt-2 border-b-4 border-gray-300 first:mt-0 break-inside-avoid">
             <section class="break-inside-avoid">
-              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 print:font-normal">EDUCATION</h2>
+              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 dark:text-slate-100 print:font-normal">EDUCATION</h2>
               <section class="mt-2 border-b-2 pb-4 break-inside-avoid">
                 <header>
-                  <h3 class="text-lg font-semibold text-gray-900 leading-snugish">University Of Liverpool</h3>
-                  <p class="leading-normal text-gray-600 text-md">2023 &ndash; Present | Master of Artificial Intelligence</p>
+                  <h3 class="text-lg font-semibold text-gray-900 dark:text-slate-100 leading-snugish">University Of Liverpool</h3>
+                  <p class="leading-normal text-gray-600 dark:text-slate-400 text-md">2023 &ndash; Present | Master of Artificial Intelligence</p>
                 </header>
               </section>
               <section class="pb-4 mt-4 mb-4 break-inside-avoid">
                 <header>
-                  <h3 class="flex-grow text-lg font-semibold text-gray-900 leading-snugish">WIT Academy</h3>
-                  <p class="leading-normal text-gray-600 text-md">2016 &ndash; 2020 | Bachelor of Computer Science</p>
+                  <h3 class="flex-grow text-lg font-semibold text-gray-900 dark:text-slate-100 leading-snugish">WIT Academy</h3>
+                  <p class="leading-normal text-gray-600 dark:text-slate-400 text-md">2016 &ndash; 2020 | Bachelor of Computer Science</p>
                 </header>
               </section>
             </section>
           </section>
           <section class="pb-6 mt-0 mb-4 border-b-4 border-gray-300 first:mt-0 break-inside-avoid">
             <section class="break-inside-avoid">
-              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 print:font-normal">WORK EXPERIENCE</h2>
+              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 dark:text-slate-100 print:font-normal">WORK EXPERIENCE</h2>
               <section class="mb-4 break-inside-avoid">
                 <header>
-                  <h3 class="flex-grow text-lg font-semibold text-gray-900 leading-snugish">Junior Architect</h3>
-                  <p class="leading-normal text-gray-600 text-md">2024 &ndash; Present | AXA</p>
+                  <h3 class="flex-grow text-lg font-semibold text-gray-900 dark:text-slate-100 leading-snugish">Junior Architect</h3>
+                  <p class="leading-normal text-gray-600 dark:text-slate-400 text-md">2024 &ndash; Present | AXA</p>
                 </header>
-                <ul class="mt-2 list-disc list-inside text-gray-800 text-md">
+                <ul class="mt-2 list-disc list-inside text-gray-800 dark:text-slate-200 text-md">
                   <li>I guide developer teams to implement better architecture and design practices, ensuring the robustness and scalability of our solutions.</li>
                   <li>I focus on enhancing the Identity and Access Management (IAM) solution.</li>
                   <li>I collaborate closely with developers to refine system designs.</li>
@@ -48,10 +48,10 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
               </section>
               <section class="mb-4 break-inside-avoid">
                 <header>
-                  <h3 class="flex-grow text-lg font-semibold text-gray-900 leading-snugish">.NET Developer</h3>
-                  <p class="leading-normal text-gray-600 text-md">07.2023 &ndash; 12.2023 | AXA</p>
+                  <h3 class="flex-grow text-lg font-semibold text-gray-900 dark:text-slate-100 leading-snugish">.NET Developer</h3>
+                  <p class="leading-normal text-gray-600 dark:text-slate-400 text-md">07.2023 &ndash; 12.2023 | AXA</p>
                 </header>
-                <ul class="mt-2 list-disc list-inside text-gray-800 text-md">
+                <ul class="mt-2 list-disc list-inside text-gray-800 dark:text-slate-200 text-md">
                   <li>As an active participant in an Agile team, I developed new features for our internal solution to meet the needs of agents and customers, while also providing ongoing support and addressing issues.</li>
                   <li>I collaborated with the business team to define appropriate technical solutions, prioritized code quality, and engaged in code reviews to ensure high-quality, maintainable, and scalable code.</li>
                   <li>As a member of the ALM/DEV team, I contributed to improving ALM processes for efficient software development and delivery.</li>
@@ -60,22 +60,22 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
               </section>
               <section class="mb-4 break-inside-avoid">
                 <header>
-                  <h3 class="flex-grow text-lg font-semibold text-gray-900 leading-snugish">Junior .NET Developer</h3>
-                  <p class="leading-normal text-gray-600 text-md">05.2022 &ndash; 06.2023 | AXA</p>
+                  <h3 class="flex-grow text-lg font-semibold text-gray-900 dark:text-slate-100 leading-snugish">Junior .NET Developer</h3>
+                  <p class="leading-normal text-gray-600 dark:text-slate-400 text-md">05.2022 &ndash; 06.2023 | AXA</p>
                 </header>
               </section>
               <section class="mb-4 break-inside-avoid">
                 <header>
-                  <h3 class="flex-grow text-lg font-semibold text-gray-900 leading-snugish">Trainee .NET Developer</h3>
-                  <p class="leading-normal text-gray-600 text-md">12.2020 &ndash; 04.2021 | AXA</p>
+                  <h3 class="flex-grow text-lg font-semibold text-gray-900 dark:text-slate-100 leading-snugish">Trainee .NET Developer</h3>
+                  <p class="leading-normal text-gray-600 dark:text-slate-400 text-md">12.2020 &ndash; 04.2021 | AXA</p>
                 </header>
               </section>
               <section class="mb-4 break-inside-avoid">
                 <header>
-                  <h3 class="flex-grow text-lg font-semibold text-gray-900 leading-snugish">.NET Developer</h3>
-                  <p class="leading-normal text-gray-600 text-md">07.2018 &ndash; 12.2020 | Freelancer</p>
+                  <h3 class="flex-grow text-lg font-semibold text-gray-900 dark:text-slate-100 leading-snugish">.NET Developer</h3>
+                  <p class="leading-normal text-gray-600 dark:text-slate-400 text-md">07.2018 &ndash; 12.2020 | Freelancer</p>
                 </header>
-                <ul class="mt-2 list-disc list-inside text-gray-800 text-md">
+                <ul class="mt-2 list-disc list-inside text-gray-800 dark:text-slate-200 text-md">
                   <li>Developed a custom system for a currency exchange bureau in Warsaw, enabling clients to seamlessly exchange bitcoin for fiat currency and vice versa through an automated process.</li>
                   <li>Worked on a reservation system for a car rental company, streamlining the booking process for their customers.</li>
                   <li>Designed and implemented an innovative online lesson platform, allowing tutors to set their availability and students to easily book lesson times. The platform facilitated seamless communication through voice, chat, and a whiteboard tool, eliminating the need for additional software.</li>
@@ -85,20 +85,20 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
           </section>
           <section class="pb-6 mt-0 mb-4 border-b-4 border-gray-300 first:mt-0 break-inside-avoid">
             <section class="break-inside-avoid">
-              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 print:font-normal">CERTIFICATES</h2>
+              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 dark:text-slate-100 print:font-normal">CERTIFICATES</h2>
               <section class="mb-0 break-inside-avoid">
                 <section class="mt-1 last:pb-1">
                   <ul class="font-bold leading-relaxed text-md -mr-1.6">
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">
                         <a class="block" href="https://learn.microsoft.com/en-gb/users/czerniawski-lukasz/credentials/6fd1278d796dbd72">Azure Fundamentals</a>
                     </li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">
                         <a class="block" href="https://learn.microsoft.com/en-gb/users/czerniawski-lukasz/credentials/35707c2a0c4d046c">Azure Data Fundamentals</a>
                     </li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">
                         <a class="block" href="https://learn.microsoft.com/en-gb/users/czerniawski-lukasz/credentials/221f77f65a806101">Azure AI Fundamentals</a>
                     </li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">
                         <a class="block" href="https://www.datacamp.com/certificate/DEA0017926458443">Data Engineer Associate</a>
                     </li>
                   </ul>
@@ -108,25 +108,25 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
           </section>
           <section class="pb-6 mt-0 mb-4 first:mt-0 break-inside-avoid">
             <section class="break-inside-avoid">
-              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 print:font-normal">TECHNOLOGIES</h2>
+              <h2 class="mb-2 text-lg font-bold tracking-widest text-gray-900 dark:text-slate-100 print:font-normal">TECHNOLOGIES</h2>
               <section class="mb-0 break-inside-avoid">
                 <section class="mt-1 last:pb-1">
                   <ul class="flex flex-wrap -mb-1 font-bold leading-relaxed text-md -mr-1.6">
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">.NET</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">C#</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">MSSQL</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Azure</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Angular</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">JavaScript</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">TypeScript</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Node.js</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Deno</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Rust</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Python</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Scikit Leatn</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">TenserFlow</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">PyTorch</li>
-                    <li class="p-1.5 mb-1 leading-relaxed text-white bg-gray-800 mr-1.6 print:bg-white print:border-inset">Keras</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">.NET</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">C#</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">MSSQL</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Azure</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Angular</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">JavaScript</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">TypeScript</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Node.js</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Deno</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Rust</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Python</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Scikit Leatn</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">TenserFlow</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">PyTorch</li>
+                    <li class="p-1.5 mb-1 leading-relaxed text-slate-100 bg-gray-800 dark:text-gray-900 dark:bg-slate-100 mr-1.6 print:bg-white print:border-inset">Keras</li>
                   </ul>
                 </section>
               </section>

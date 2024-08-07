a49eee37283bc5d4b7741a58216b7471a11c1e72
Lukasz Czerniawski <l.czerniawski@icloud.com>
2024-08-07 19:59:03 +0200

    Add footer

--- _includes/main_layout.tsx
+++ _includes/main_layout.tsx
@@ -35,10 +35,20 @@ export default (data: Lume.Data, _helpers: Lume.Helpers) => {
             </ul>
           </nav>
         </header>
-        <div class="h-full">
+        <div class="flex-grow">
           {data.children}
         </div>
-        <button onClick="toggleTheme()" class="fixed bottom-4 right-4 px-2 py-2 bg-orange-600 text-white rounded-lg shadow-lg hover:bg-orange-500 focus:outline-none">
+        <footer class="flex flex-col w-3/6">
+          <hr class="border-gray-600 dark:border-slate-100" />
+          <p class="w-full text-center my-6 text-gray-900 dark:text-slate-100">
+            © 2024 Łukasz Czerniawski. Powered by{" "}
+            <a href="https://lume.land/">🔥Lume</a>
+          </p>
+        </footer>
+        <button
+          onClick="toggleTheme()"
+          class="fixed bottom-4 right-4 px-2 py-2 bg-orange-600 text-white rounded-lg shadow-lg hover:bg-orange-500 focus:outline-none"
+        >
           <svg
             width="24"
             height="24"
@@ -47,9 +57,7 @@ export default (data: Lume.Data, _helpers: Lume.Helpers) => {
             xmlns="http://www.w3.org/2000/svg"
             class="dark:hidden"
           >
-            <path
-              d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z"
-            >
+            <path d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z">
             </path>
           </svg>
           <svg

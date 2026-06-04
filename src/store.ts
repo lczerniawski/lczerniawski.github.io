3471f2efcd4671ffa925858dbd20bdcf5579676b
Lukasz Czerniawski <l.czerniawski@icloud.com>
2026-06-04 11:11:50 +0200

    feat: add animation for terminal prompt when changing pages

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,6 @@
-import { atom } from 'nanostores';
+import { atom } from "nanostores";
 
-export const searchQuery = atom('');
+export const searchQuery = atom("");
 export const isMainCursorVisible = atom(true);
+export const isNavigationOngoing = atom(false);
+export const lastNavigationPathname = atom("");
